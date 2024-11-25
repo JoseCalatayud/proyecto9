a2a22499ceda634fba740915fa642b13fba8a9a9
curso05 <notiene@notiene.es>
2024-11-25 12:04:53 +0100

    Inicializacion de repositorio

---
+++ src/main/java/es/santander/ascender/proyecto9/Proyecto9Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.proyecto9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto9Application.class, args);
+	}
+
+}
