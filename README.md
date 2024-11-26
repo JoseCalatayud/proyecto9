c62237e4d4602e1d17c62eff7b9d228ce0b7faa6
curso05 <notiene@notiene.es>
2024-11-26 12:09:23 +0100

    Peticiones funcionando

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # proyecto09
+
+Cuando quieras probarlo para leer un registro
+curl 'http://localhost:8080/solicitud/5'
+
+Cuando quieras listar
+curl 'http://localhost:8080/solicitud'
+
+Cuando quieras borrar
+curl 'http://localhost:8080/solicitud/5' -X DELETE
+
+Cuando quieras crear sin ID
+curl 'http://localhost:8080/solicitud' -X POST -H 'Content-Type: application/json'  --data-raw '{"id":1,"nombre":"Antonio","edad":34}'
--- src/main/java/es/santander/ascender/proyecto9/controller/SolicitudController.java
+++ src/main/java/es/santander/ascender/proyecto9/controller/SolicitudController.java
@@ -1,5 +1,6 @@
 package es.santander.ascender.proyecto9.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,35 +8,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import es.santander.ascender.proyecto9.Solicitud;
 
 @RestController
 @RequestMapping ("/solicitud")
 public class SolicitudController {
     @PostMapping
-    public long crear(Solicitud solicitud) {
-        throw new UnsupportedOperationException("Pdte");
+    public long crear(@RequestBody Solicitud solicitud) throws Exception {
+        if (solicitud.getId()!= 0){
+            throw new Exception("Al crear no me puedes pasar el ID");
+        }
+        return 16;
+        
     }
     @GetMapping ("/{id}")
     public Solicitud leer(@PathVariable long id) {
-        throw new UnsupportedOperationException("Pdte");
+        Solicitud respuesta = new Solicitud();
+
+        respuesta.setId(id);
+        respuesta.setNombre("Jose");
+        respuesta.setEdad(44);
+        return respuesta;
     }
     @GetMapping
     public List<Solicitud> listar() {
-
-        throw new UnsupportedOperationException("Pdte");
+        List <Solicitud>lista = new ArrayList<>();
+        for (int i = 1; i < 15; i++) {
+            Solicitud respuesta = new Solicitud();
+            respuesta.setId(i);
+            respuesta.setEdad(33+i);
+            respuesta.setNombre("Jose "+i);
+            lista.add(respuesta);
+            
+        }
+        return lista;
     }
     @PutMapping
-    public void modificar (Solicitud solicitud) {
-        throw new UnsupportedOperationException("Pdte");
+    public void modificar (@RequestBody Solicitud solicitud) throws Exception {
+        if (solicitud.getId()== 0){
+            throw new Exception("Al modificar me tienes que pasar un ID existente");
+        }
+        
     }
     @DeleteMapping ("/{id}")
-    public void borrar (long id){
-        throw new UnsupportedOperationException("Pdte");
+    public void borrar (@PathVariable long id){
+        
     }
 
 }
